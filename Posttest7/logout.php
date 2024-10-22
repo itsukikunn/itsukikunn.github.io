71a7c9ac6c962f2ead83e134fbef6b44d5ae1366
itsukikunn <2309106025@student.unmul.ac.id>
2024-10-22 13:54:41 +0800

    Delete Posttest7 directory

--- Posttest7/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-$_SESSION['logout_message'] = 'Anda telah berhasil logout!';
-session_unset();
-session_destroy();
-
-header("Location: index.php?status=logout");
-exit;
-?>
